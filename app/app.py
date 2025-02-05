0784277a7e7353caa1e4a8a8841789c63890bf31
AlexLnv <a.lunyakov@my.com>
2025-02-05 17:08:59 +0300

    rt-detr model predict

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 import os
 import uuid
 import time
+import numpy as np
 import gradio as gr
 import pandas as pd
 import webbrowser, os
@@ -26,26 +27,68 @@ custom_theme = gr.themes.Monochrome(
     button_small_radius='*radius_xl',
 )
 
-def warning_file():
-    gr.Warning("Выберите файл для распознавания!")
 
-def info_fn():
+def warning(stype: str):
+    """
+    Предупреждение пользователя
+    :параметр stype: тип ошибки (строка)
+    ------------------
+    User Warning
+    :param stype: error type (string)
+    """
+    gr.Warning("Выберите файл для распознавания!") if stype == 'file' else gr.Warning("Выберите модель распознавания!")
+
+
+def inf():
+    """
+    Информация для пользователя, инструкция по работе с системой (так как я не смог вызвать функцию с параметром в виде строки с кнопки)
+    ------------------
+    Information for the user, instructions for working with the system (since I could not call the function with a param in the form of a string from the button)
+    """
     gr.Info("Для начала работы - загрузите ваши файлы в формате jpg, jpeg, png")
 
-def info_req():
-    # startup_conf()
-    gr.Info("Распознавание может занять некоторое время")
-    
-def info_res():
-    gr.Info("Посмотреть обработанные файлы можно, нажав на кнопку ниже")
+
+def inform(stype: str):
+    """
+    Информация для пользователя, инструкция по работе с системой
+    :параметр stype: тип информации (строка)
+    ------------------
+    User information, instructions for working with the system
+    :param stype: information type (string)
+    """
+    if stype == 'working':
+        gr.Info("Распознавание может занять некоторое время")
+    elif stype == 'end':
+        gr.Info("Посмотреть обработанные файлы можно, нажав на кнопку ниже")
+    else:
+        gr.Info("Как ты это сделал? Ачивку в студию")
+   
 
 def shorten_filename(filename, max_length):
-    """Сокращение имени файла до max_length символов и добавление «...» в конец"""
+    """
+    Сокращение имени файла до max_length символов и добавление «...» в конец
+    :параметр filename: название оригинального файла (строка)
+    :параметр max_length: максимальная длина названия файла (целое)
+    ------------------
+    Shorten file name to max_length characters and add "..." to the end
+    :param filename: name of original file (string)
+    :param max_length: maximum length of file name (integer)
+    """
     if len(filename) > max_length:
         return filename[:max_length] + '...'
     return filename
 
+
 def create_confidence_plots(stats_df):
+    """
+    Создание инфографики на основе средних значений достоверности
+    :параметр stats_df: pd.Dataframe с результатами детекции
+    :return fig: график plotly
+    ------------------
+    Creating infographics based on average confidence values
+    :stats_df param: pd.Dataframe with detection results
+    :return fig: plotly graph
+    """
     file_names = stats_df['file_name'].unique()
     fig = make_subplots(rows=1, cols=len(file_names), 
                         subplot_titles=file_names,
@@ -61,17 +104,23 @@ def create_confidence_plots(stats_df):
                      marker_color='#0A7153')
         
         fig.add_trace(bar, row=1, col=i)
-        fig.update_traces(width=len(file_data['class_name']) * 0.3)
+        fig.update_traces(width=0.35)
         fig.update_yaxes(range=[0, 1], row=1, col=i)
     
     fig.update_layout(
         height = 400, 
-        width = 350 * len(file_names), 
-        title_text="Средний уровень достоверности по классам для каждого файла",
+        width = 400 * len(file_names),
+        title={
+            'text': "Средний уровень достоверности по классам",
+            'y':0.9,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'
+        },
         showlegend=False,
         autosize=True,
-        paper_bgcolor='#1f2937',
-        plot_bgcolor='#1f2937',
+        paper_bgcolor='#0f172a',
+        plot_bgcolor='#0f172a',
         font=dict(color='#ffffff'),
         xaxis=dict(tickangle=45),
     )
@@ -90,13 +139,31 @@ def create_confidence_plots(stats_df):
     
     return fig
 
+
 def fileOpen():
+    """
+    Переход к папке последней детекции через проводник системы
+    ------------------
+    Go to the last detection folder via the system explorer
+    """
     webbrowser.open(os.path.realpath(str(newfolder)))
 
-def photoProcessing(files, ):
-    time.sleep(1)
-    if files is not None:
-        info_req()
+
+def photoProcessing(files, cv_model):
+    """
+    Проверка предоставленных пользователем фотографий
+    :параметр files: пути до файлов (массив строк)
+    :параметр cv_model: номер выбранной модели (целое)
+    :return detections_files, df, confidence_plots: изображения с bbox, таблица результатов, графики распределения
+    ------------------
+    Checking user-provided photos
+    :param files: paths to files (array of strings)
+    :param cv_model: number of the selected model (integer)
+    :return detections_files, df, confidence_plots: images with bbox, results table, distribution graphs
+    """
+    if files is not None and cv_model is not None:
+        inform('working')
+        time.sleep(1)
         
         folder_name = str(uuid.uuid4())
         output_folder = os.path.join('./files', folder_name)
@@ -105,7 +172,8 @@ def photoProcessing(files, ):
         os.makedirs(output_folder, exist_ok=True)
 
         detections_files = []
-        df = pd.DataFrame(columns=['file_name', 'class_name', 'confidence'])
+        class_list = ['Coverall', 'Face_Shield', 'Gloves', 'Goggles', 'Mask']
+        df = pd.DataFrame(columns=['file_name', 'class_id', 'class_name', 'confidence'])
         pd.set_option('display.precision', 3)
         
         for elem in files:
@@ -114,17 +182,25 @@ def photoProcessing(files, ):
             dfilename = os.path.join(output_folder, f'd_{filename}.jpg')
             detections_files.append(dfilename)
             
-            detections_list = ppe_detection(elem, dfilename)
-            img_class_names = detections_list.data['class_name']
+            detections_list = img_ppe_detection(elem, dfilename, model_type=cv_model)
             img_detections = detections_list.confidence
+            img_class_id = detections_list.class_id
+            img_class_names = []
             
-            for class_name, confidence in zip(img_class_names, img_detections):
+            if cv_model:
+                img_class_names = detections_list.data['class_name']
+            else: 
+                for i in range(len(detections_list.class_id)):
+                    img_class_names.append(class_list[detections_list.class_id[i]])
+
+            for class_name, class_id, confidence in zip(img_class_names, img_class_id, img_detections):
                 new_row = pd.DataFrame({
                     'file_name': [filename],
+                    'class_id': [class_id],
                     'class_name': [class_name],
                     'confidence': [round(float(confidence), 3)]
                 })
-                df = pd.concat([df, new_row], ignore_index=True)
+                df = pd.concat([df if not df.empty else None, new_row], ignore_index=True)
         
         detections_file_path = os.path.join(output_folder, 'detections.csv')
         df.to_csv(detections_file_path, sep=',', index=False)
@@ -133,23 +209,37 @@ def photoProcessing(files, ):
         df['file_name'] = df['file_name'].apply(lambda x: shorten_filename(x, 10))
         
         # Подсчет статистики
-        file_stats_df = df.groupby(['file_name', 'class_name'])['confidence'].agg(['mean', 'max', 'min', 'count']).reset_index()
-        file_stats_df.columns = ['file_name', 'class_name', 'mean_confidence', 'max_confidence', 'min_confidence', 'detection_count']
+        file_stats_df = df.groupby(['file_name', 'class_id', 'class_name'])['confidence'].agg(['mean', 'max', 'min', 'count']).reset_index()
+        file_stats_df.columns = ['file_name', 'class_id', 'class_name', 'mean_confidence', 'max_confidence', 'min_confidence', 'detection_count']
         file_stats_df[['mean_confidence', 'max_confidence', 'min_confidence']] = file_stats_df[['mean_confidence', 'max_confidence', 'min_confidence']].round(3)
         file_stats_df = file_stats_df.sort_values(['file_name', 'mean_confidence'], ascending=[True, False])
-        
         confidence_plots = create_confidence_plots(file_stats_df)
-        
-        info_res()
+        inform('end')
         return detections_files, df, confidence_plots
+    
+    elif cv_model is None:
+        warning('radio')
+        return None, None, None
     else:
-        warning_file()
-        return None, None
+        warning('file')
+        return None, None, None
 
-def videoProcessing(file, ):
+
+def videoProcessing(file, cv_model):
+    """
+    Проверка предоставленного пользователем видео
+    :параметр file: путь до файла (строка)
+    :параметр cv_model: номер выбранной модели (целое)
+    :return dfilename, df: путь до видео с bbox, таблица результатов
+    ------------------
+    Checking user-provided video
+    :param file: path to file (string)
+    :param cv_model: number of selected model (integer)
+    :return dfilename, df: path to video from bbox, results table
+    """
     time.sleep(1)
-    if file is not None:
-        info_req()
+    if file is not None and cv_model is not None:
+        inform('working')
         
         folder_name = str(uuid.uuid4())
         output_folder = os.path.join('./files', folder_name)
@@ -160,17 +250,19 @@ def videoProcessing(file, ):
         filename, _ = os.path.splitext(file.split('\\')[-1])
         dfilename = os.path.join(output_folder, f'd_{filename}.mp4v')
         dcsvfilename = os.path.join(output_folder, 'detections.csv')
-        print(dfilename)
 
-        video_ppe_detection(source=file, result_name=dfilename, output_folder=output_folder)
-        info_res()
+        video_ppe_detection(source=file, result_name=dfilename, output_folder=output_folder, model_type=cv_model)
+        inform('end')
         
         df = pd.read_csv(dcsvfilename)
         
         return dfilename, df
+    elif cv_model is None:
+        warning('radio')
+        return None, None
     else:
-        warning_file()
-        return None, None, None
+        warning('file')
+        return None, None
 
 
 custom_css = """
@@ -197,7 +289,17 @@ custom_css = """
 }
 """
 
+
 def theme(new_theme):
+    """
+    Изменение темы светлая\темная
+    :параметр new_theme: путь до файла (строка)
+    :return toggle_theme: переключение темы
+    ------------------
+    Change theme light\dark
+    :param new_theme: path to file (string)
+    :return toggle_theme: switch theme
+    """
     if new_theme == 'dark':
         toggle_theme = """
         function refresh() {
@@ -216,6 +318,7 @@ def theme(new_theme):
         }
         """
     return toggle_theme
+
 
 theme_btns = """
     function hide_btns() {
@@ -242,6 +345,7 @@ theme_btns = """
     }
 """
 
+
 with gr.Blocks(theme=custom_theme, css=custom_css, js=theme_btns) as demo:
 
     toggle_button = gr.Button("☾", elem_id='theme-toggle')
@@ -257,18 +361,19 @@ with gr.Blocks(theme=custom_theme, css=custom_css, js=theme_btns) as demo:
 
     with gr.Row():
         with gr.Column():
-            # gr.Markdown("""<p align="start"><font size="4px">Детекция PPE<br></font></p>""")
             with gr.Tab('Изображение'):
-                files_photo = gr.File(label="Загрузите ваши файлы здесь", file_types=['.png','.jpeg','.jpg'], file_count='multiple')
+                with gr.Row(equal_height=True):
+                    files_photo = gr.File(label="Загрузите ваши файлы здесь", file_types=['.png','.jpeg','.jpg'], file_count='multiple', scale=7)
+                    cv_model_img = gr.Radio(["RT-DETR-L", "YOLOv11-M"], label="Модель", scale=1, type='index')
                 with gr.Column():
                     with gr.Row():
                         btn_photo = gr.Button(value="Начать распознавание",)
-                        triggerImage = gr.Button(value="Подробнее",)
+                        tmp = 'end'
+                        InfoImage = gr.Button(value="Подробнее")
                 with gr.Row():
                         with gr.Row('Результат обработки', equal_height=True):
                             with gr.Column(variant='panel',):
                                 predictImage = gr.Gallery(type="filepath", label="Предсказание модели", columns=[2], rows=[1], preview=True, allow_preview=True, object_fit="contain", height=500)
-                                # statsDF = gr.DataFrame(headers=['Статистика по классам'],)
                                 statsPLOT = gr.Plot(label='Распределение найденных классов', elem_id='stats-plot', container=True)
                             with gr.Column():
                                 gr.Markdown("""<p align="start"><font size="4px">Что происходит в данном блоке?<br></p>
@@ -281,15 +386,17 @@ with gr.Blocks(theme=custom_theme, css=custom_css, js=theme_btns) as demo:
                                             </ul></font>""")
                                 predictImageClass = gr.DataFrame(headers=["Результаты обработки"], elem_id='dataframe')
             with gr.Tab('Видео'):
-                file_video = gr.File(label="Загрузите ваши файлы здесь", file_types=['.mp4','.mkv'], file_count = 'single')
+                with gr.Row(equal_height=True):
+                    file_video = gr.File(label="Загрузите ваши файлы здесь", file_types=['.mp4','.mkv'], file_count = 'single', scale=7)
+                    cv_model_vid = gr.Radio(["RT-DETR-L", "YOLOv11-M"], label="Модель", scale=1, type='index')
                 with gr.Column():
                     with gr.Row(): 
                         btn_video = gr.Button(value="Начать распознавание",)
-                        triggerVideo = gr.Button(value="Подробнее",)
+                        InfoVid = gr.Button(value="Подробнее",)
                 with gr.Row(equal_height=True):
                     with gr.Column(variant='panel',):
                         predictVideo = gr.Video(label="Обработанное видео", interactive=False)
-                        statsPLOT = gr.Plot(label='Сделать график время-класс', elem_id='stats-plot-2', container=True)
+                        hz = gr.Plot(label='Сделать график время-класс', elem_id='stats-plot-2', container=True)
                     with gr.Column():
                         gr.Markdown("""<p align="start"><font size="4px">Что происходит в данном блоке?<br></p>
                                     <ul><font size="3px">
@@ -303,16 +410,16 @@ with gr.Blocks(theme=custom_theme, css=custom_css, js=theme_btns) as demo:
                                                         
     with gr.Row(): 
         with gr.Row(): 
-            clr_btn = gr.ClearButton([files_photo, predictImage, predictImageClass, statsPLOT, file_video, predictVideo, predictVideoClass], value="Очистить контекст",)
+            clr_btn = gr.ClearButton([files_photo, predictImage, predictImageClass, statsPLOT, file_video, predictVideo, predictVideoClass, cv_model_img, cv_model_vid, InfoImage], value="Очистить контекст",)
             btn2 = gr.Button(value="Посмотреть файлы",)
     
     with gr.Row():
         gr.Markdown("""<p align="center">Выполнил Луняков Алексей, студент ИКБО-04-21</p>""")
-
-    btn_photo.click(photoProcessing, inputs=[files_photo, ], outputs=[predictImage, predictImageClass, statsPLOT,])
-    btn_video.click(videoProcessing, inputs=[file_video, ], outputs=[predictVideo, predictVideoClass,])
+        
+    btn_photo.click(photoProcessing, inputs=[files_photo, cv_model_img], outputs=[predictImage, predictImageClass, statsPLOT,])
+    btn_video.click(videoProcessing, inputs=[file_video, cv_model_vid], outputs=[predictVideo, predictVideoClass,])
     btn2.click(fileOpen)
-    triggerImage.click(info_fn)
-    triggerVideo.click(info_fn)
+    InfoImage.click(inf) 
+    InfoVid.click(inf) 
 
 demo.launch(allowed_paths=["/assets/"])
--- app/processing.py
+++ app/processing.py
@@ -1,37 +1,53 @@
 import os
 import cv2
 import csv
-from ultralytics import YOLO
+from ultralytics import YOLO, RTDETR
 import supervision as sv
 
-model = YOLO('./yolo11m_20.pt')
-# model = YOLO('./rtdetrl_10.pt')
+model_1 = YOLO('./yolo11m_20.pt')
+model_0 = RTDETR('./rtdetrl_10.pt')
 
 # ----------------------------- image processing -----------------------------
 
-def ppe_detection(source: str, result_name: str, conf_threshold: float=0.4) -> None:
+def img_ppe_detection(source: str, result_name: str, conf_threshold: float=0.4, model_type: int=1) -> None:
     """
     Получение предсказания модели по входной фотографии
     :параметр source: путь до оригинального файла (строка)
     :параметр result_name: путь до нового файла (строка)
     :return: лист предсказанний модели в формате supervision
     """
-    # YOLO
-    predict = model(source)[0]
-    predict.boxes[predict.boxes.conf >= conf_threshold]
-
-    detections = sv.Detections.from_ultralytics(predict)
+    
+    label_annotator = sv.LabelAnnotator()
+    bbox_annotator = sv.BoxAnnotator()
+    annotated_image = cv2.imread(source)
+    
+    if model_type:
+        # YOLO
+        predict = model_1(source)[0]
+        predict.boxes[predict.boxes.conf >= conf_threshold]
+        detections = sv.Detections.from_ultralytics(predict)
+        annotated_image = label_annotator.annotate(scene=annotated_image, detections=detections)
+        
+    else:
+        # RT-DETR
+        results = model_0(source)[0]
+        detections = sv.Detections(
+            xyxy=results.boxes.xyxy.cpu().numpy(), 
+            confidence=results.boxes.conf.cpu().numpy(),
+            class_id=results.boxes.cls.cpu().numpy().astype(int))
+        annotated_image = label_annotator.annotate(
+            scene=annotated_image, 
+            detections=detections,
+            labels=[model_0.model.names[class_id] for class_id in detections.class_id]
+        )
+        
     mask = detections.confidence >= conf_threshold
     detections = detections[mask]
-
-    label_annotator = sv.LabelAnnotator()
-    bounding_box_annotator = sv.BoxAnnotator()
     
-    annotated_image = cv2.imread(source)
-    annotated_image = bounding_box_annotator.annotate(scene=annotated_image, detections=detections)
-    annotated_image = label_annotator.annotate(scene=annotated_image, detections=detections)
+    annotated_image = bbox_annotator.annotate(scene=annotated_image, detections=detections)
+    
     cv2.imwrite(result_name, annotated_image)
-   
+    # print(detections)
     return detections
 
 # ----------------------------- video processing -----------------------------
@@ -45,9 +61,9 @@ def ms_to_time(ms):
     return f"{hours:02d}:{minutes:02d}:{seconds:02d}.{ms:03d}"
 
 
-def video_ppe_detection(source: str, result_name: str, output_folder: str, conf_threshold: float=0.4) -> None:
+def video_ppe_detection(source: str, result_name: str, output_folder: str, conf_threshold: float=0.5, model_type: int=1) -> None:
     """
-    Получение интервалов детекции СИЗ по видео
+    Получение предсказания модели по входному видео
     :параметр source: путь до оригинального файла (строка)
     :параметр result_name: путь до нового файла (строка)
     """
@@ -67,26 +83,39 @@ def video_ppe_detection(source: str, result_name: str, output_folder: str, conf_
         csv_writer.writerow(['time', 'found_classes'])
         
         last_class_count = None
+        label_annotator = sv.LabelAnnotator()
+        bbox_annotator = sv.BoxAnnotator()
         
         while video.isOpened():
             success, frame = video.read()
             if not success: break
             
-            predict = model(frame)[0]
-            predict.boxes[predict.boxes.conf >= conf_threshold]
+            if model_type:
+                predict = model_1(frame)[0]
+                predict.boxes[predict.boxes.conf >= conf_threshold]
+                detections = sv.Detections.from_ultralytics(predict)
+                mask = detections.confidence >= conf_threshold
+                detections = detections[mask]
+                frame = label_annotator.annotate(scene=frame, detections=detections)
+            else:
+                predict = model_0(frame)[0]
+                detections = sv.Detections(
+                    xyxy=predict.boxes.xyxy.cpu().numpy(), 
+                    confidence=predict.boxes.conf.cpu().numpy(),
+                    class_id=predict.boxes.cls.cpu().numpy().astype(int))
+                mask = detections.confidence >= conf_threshold
+                detections = detections[mask]
+                frame = label_annotator.annotate(
+                    scene=frame, 
+                    detections=detections,
+                    labels=[model_0.model.names[class_id] for class_id in detections.class_id]
+                )
             
             current_time_ms = video.get(cv2.CAP_PROP_POS_MSEC)
             timestamp_str = ms_to_time(current_time_ms)
             
-            detections = sv.Detections.from_ultralytics(predict)
-            mask = detections.confidence >= conf_threshold
-            detections = detections[mask]
+            frame = bbox_annotator.annotate(scene=frame, detections=detections)
             
-            label_annotator = sv.LabelAnnotator()
-            bounding_box_annotator = sv.BoxAnnotator()
-            
-            frame = bounding_box_annotator.annotate(scene=frame, detections=detections)
-            frame = label_annotator.annotate(scene=frame, detections=detections)
             cv2.putText(frame, timestamp_str, (10, 30),  cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
             result.write(frame)
@@ -95,10 +124,9 @@ def video_ppe_detection(source: str, result_name: str, output_folder: str, conf_
                 class_ids = predict.boxes.cls.cpu().numpy().astype(int)
                 unique_classes = set(class_ids)
                 current_count = len(unique_classes)
-                print(current_count)
                 
                 if current_count != last_class_count:
-                    class_names = [model.names[c_id] for c_id in unique_classes]
+                    class_names = [model_1.names[c_id] for c_id in unique_classes]
                     csv_writer.writerow([timestamp_str, ', '.join(class_names)])
                     last_class_count = current_count
 
